22cf321bd76d72a916aa6e8c999736606a8d5d31
Dylan Matthews <50dylanmatthews@gmail.com>
2017-11-05 21:23:04 +0200

    Wrong Code, uploaded working assignment 2

--- Orginal/Assigment 2/lockstockand/app/src/main/java/Journey/IJourneyFinderListener.java
+++
@@ -1,9 +0,0 @@
-package Journey;
-import java.util.List;
-
-//this code was taken ands adapted from https://github.com/hiepxuan2008/GoogleMapDirectionSimple/tree/master/app/src/main/java/Modules
-
-public interface IJourneyFinderListener {
-    void onDirectionFinderStart(); //calls method onDirectionFinderStart
-    void onDirectionFinderSuccess(List<Journey> route);//list of jounrney route getting sent to onDirectionFinderSuccess
-}
